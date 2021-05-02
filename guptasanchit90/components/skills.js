85f3962f524ebd590d4881306d8937d40ab26724
Sanchit Gupta <gupta.sanchit90@gmail.com>
2021-05-02 17:46:06 +0530

    Skill details

--- guptasanchit90/components/skills.js
+++ guptasanchit90/components/skills.js
@@ -9,31 +9,37 @@ const skills = [
     {
         category: "Front-End Development",
         icon: faLaptopCode,
+        description: "Exprienced in building web applications using React, Angular along with help of HTML, CSS following modern web development standards",
         technologyAndTools: ['Javascript', 'React', 'Angular', 'HTML', 'CSS']
     },
     {
         category: "Back-End Development",
         icon: faServer,
+        description: "Over the years I have worked on a wide range of Applications using NodeJS with help ExpressJS, Socket.IO, GraphQL and other awesome Modules.",
         technologyAndTools: ['Node JS', 'Java', 'PHP']
     },
     {
         category: "Database",
         icon: faDatabase,
+        description: "Databases I have extensively used in the past include MySQL, MongoDB, Postgres and FireStore",
         technologyAndTools: ['MySQL', 'MongoDB', 'Firebase', 'Firestore']
     },
     {
-        category: "Version Control",
+        category: "Dev-Ops",
         icon: faCodeBranch,
+        description:"Tools like Docker, Kubernetes, GIT, Jenkins have become part of my daily work.",
         technologyAndTools: ['GIT', 'Subversion']
     },
     {
         category: "Project Management",
         icon: faTasks,
+        description:"With experince come's resposiblity, over the year I have mentored team of different size going upto 18 developers",
         technologyAndTools: ['JIRA', 'Gitlab', 'Trello', 'Aha', 'Confluence']
     },
     {
         category: "Mobile Development",
         icon: faMobileAlt,
+        description:"Over the years I have worked on countless Mobile applications using frameworkes like Cordova, React Native and many more",
         technologyAndTools: ['Android', 'iOS', 'Cordova', 'React Native']
     }
 ]
@@ -58,8 +64,7 @@ export default function Skills() {
                                     <Card.Title>{skill.category}</Card.Title>
                                 </Card.Body>
                                 <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
+                                    {skill.description}
                                 </Card.Text>
                             </Card>
                         </Col>
